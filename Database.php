0a92c932495fbdd857c7e390d0926d153bd0fd35
S.M.Imtiaz <smimtiaz.cse@gmail.com>
2017-10-21 22:22:17 +0600

    saved after creating Read Function

---
+++ Database.php
@@ -0,0 +1,40 @@
+<?php
+
+class Database{
+	public $host= DB_HOST;
+	public $user= DB_USER;
+	public $pass= DB_PASS;
+	public $dbname= DB_NAME;
+
+	public $link;
+	public $error;
+
+
+//Connect to Database
+	public function __construct(){
+		$this->connectDB();
+	}
+
+//Create Database connection
+	public function connectDB(){
+		$this->link = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+		if(!$this->link){
+			$this->error ="Connection Failed".$this->link->connect_error;
+			return false;
+		}
+	}
+
+
+//Read Data From Database;
+	public function select($query){
+		$result = $this->link->query($query) or die($this->link->error.__LINE__);
+		if($result->num_rows > 0){
+			return $result;
+		} else{
+			return false;
+		}
+	}
+
+
+
+}
--- index.php
+++ index.php
@@ -1,20 +1,34 @@
-<?php include 'inc/header.php'; ?>
+<?php 
+include 'inc/header.php'; 
+include 'config.php';
+include 'Database.php';
+?>
+<?php
+	$db = new Database();
+	$query = "SELECT * FROM tbl_user";
+	$select=$db->select($query);
+
+?>
 		
 <table class="tblone">
 	<tr>
-		<th width="25%">Name</th>
+		<th width="35%">Name</th>
 		<th width="25%">E-mail</th>
-		<th width="25%">Skill</th>
+		<th width="15%">Skill</th>
 		<th width="25%">Action</th>		
 	</tr>
-
+<?php if($select){ 
+		while($read = $select->fetch_assoc()){
+	?> 
 	<tr>
-		<td>Name</td>
-		<td>E-mail</td>
-		<td>Skill</td>
-		<td><a href="update.php?id=1">Edit</a></td>
+		<td> <?php echo $read['name']; ?> </td>
+		<td> <?php echo $read['email']; ?> </td>
+		<td> <?php echo $read['skill']; ?> </td>
+		<td><a href="update.php?id= <?php echo $read['id']; ?> ">Edit</a></td>
 	</tr>
-
+<?php }
+	}
+	?>
 </table>
 		
 
