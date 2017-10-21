44671a6ae3035b913f1d603dace0014650159431
S.M.Imtiaz <smimtiaz.cse@gmail.com>
2017-10-21 21:46:48 +0600

    After Database connection

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'inc/header.php'; ?>
+		
+<table class="tblone">
+	<tr>
+		<th width="25%">Name</th>
+		<th width="25%">E-mail</th>
+		<th width="25%">Skill</th>
+		<th width="25%">Action</th>		
+	</tr>
+
+	<tr>
+		<td>Name</td>
+		<td>E-mail</td>
+		<td>Skill</td>
+		<td><a href="update.php?id=1">Edit</a></td>
+	</tr>
+
+</table>
+		
+
+
+
+
+
+
+
+
+
+		
+
+<?php include 'inc/footer.php'; ?>
